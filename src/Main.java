f64df7e920b7d5085457849b17a7a92ddb253039
Juan Abreu <juanvsabreu@gmail.com>
2022-10-10 22:40:31 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,38 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-    }
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);,
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJuan = new Dev();
+        devJuan.setNome("Juan");
+        devJuan.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Juan " + devJuan.getConteudosInscritos());
+        devJuan.progredir();
+        devJuan.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Juan " + devJuan.getConteudosConcluidos());
+        System.out.println("XP: " + devJuan.calcularTotalXp());
+
+        System.out.println("------");
+
+        Dev devVictor = new Dev();
+        devVictor.setNome("Victor");
+        devVictor.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Victor " + devVictor.getConteudosInscritos());
+        devVictor.progredir();
+        devVictor.progredir();
+        devVictor.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Victor " + devVictor.getConteudosConcluidos());
+        System.out.println("XP: " + devVictor.calcularTotalXp());
+     }
 }
